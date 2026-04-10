aa85dc7a0dca36761f92e1a6f247c2134d6f7d64
BinhPhamTieSinger <79978921+BinhPhamTieSinger@users.noreply.github.com>
2026-04-10 15:06:14 +0700

    Add solution for eigenvalues

---
+++ eigenvalues/eigenvalues.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def calculate_eigenvalues(matrix):
+    """
+    Calculate eigenvalues of a square matrix.
+    """
+    # Write code here
+    try:
+        matrix = np.array(matrix)
+        return np.linalg.eigvals(matrix)
+    except:
+        return None
